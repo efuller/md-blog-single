d30d3476d1ac81bdc1a6d9334577a10b10ccc280
ModernTek <nvaslav48@verizon.net>
2017-01-11 12:31:34 -0500

    Add first version of data object

---
+++ blog.js
@@ -0,0 +1,140 @@
+var data = {
+        		"title": "Blog Modern Developer",
+        		"description": "This is Blog for Modern Developer",
+				
+				"blogger-time": "2 days ago",
+		 		"blogger-image": "images/paul.png",
+		 		"blogger-name": "Paul Irish",
+				
+				"main-content":"We're always looking for ways to improve site speed; we lazy load, minify and concatenate, create sprites, experiment with caching, and just about everything else we can think of The idea behind image optimization is simple but the execution hasn't always been: each of those optimizations usually requires a separate solution which makes automation difficult. Enter Cloudinary: uploading, storing, manipulating, and delivering images and videos on the web",                   
+		 		"main-image":	"images/featured-image.png",	 
+				   
+				 "related_articles": [
+		   			{
+		   	    		"link": "#",
+		   				"decsription": "5 More Awesome New Mozilla Technologies You've Never Heard Of"
+		   			},
+		   			{
+		  				"link": "#",
+		   				"description": "Git Hook  npm install if package.json is Modified"
+		   			},
+		   			{
+		  				"link": "http",
+		   				"descrition":"Complete server Stability and Scurity with Plesk"
+		   			},
+		   			{
+		  				"link": "#",
+		   				"description": "Use Mozilla Firefox DevTools in Chrome"
+		   			},
+		   			{
+		  				"link": "#",
+		   				"description": "Great Tools to Use in Your Projects"
+		   			}
+				 ],
+				 "postComment":
+					{
+		  				"commentId": "1",
+		  				"commentAuthor": "Current User",
+		  				"coomentDate": "todays date",
+		  				"commentText": "lorem"
+					},
+		
+				 "comments": [
+		   			{
+		  				"comment-author-name": "Rory Duncan",
+		  				"comment-author-image":"images/rory.png",
+		  				"comment-time": "1 days ago",
+		  				"comment-content":"do you know of any examples"
+		 			},
+		 			{ 
+		 				"comment-author-name":"Peter Chang",
+		  				"comment-author-image": "image/peter.png",
+		   				"comment-time": "2 days ago",
+		   				"comment-content": "I never heard about it"
+		 			}
+		
+		         ],//comments
+		
+		         "recent-articles": [
+		 			{
+		   				"article-link": "#",
+		   				"article-description": "Getting Your Selected Articles Published on Encyclopedia",
+		   				"article-time": "a day ago"
+		 			},
+
+		 			{
+		   				"article-link": "#",
+		   				"article-description": "Welcome to Encyclopedia Modern Developer",
+		   				"article-time": "2 days ago"
+		 			},
+		   		    {
+		   				"article-link": "#",
+		   				"article-description": "All Trappings",
+		   				"article-time": "12 days ago"
+		 			},
+		   				"article-link": "#",
+		   				"article-description": "Getting Your Selected Articles Published on Encyclopedia",
+		   				"article-time":" 33 days ago"
+		 			},
+		 			{
+		   				"article-link": "#",
+		   				"article-description": "Welcome to Encyclopedia Modern Developer",
+		   				"article-time": "54 days ago"
+		 			},
+		 			{
+		   				"article-link": "#",
+		   				"article-description": "All Trappings",
+		   				"article-time": "68 days ago"
+		 			}
+	  			 ],//recent articles
+				 "upcoming_posts"[
+		   			{
+						"article-link": "#",
+		  				 "article-description": "Getting Your Selected Articles Published on Encyclopedia"
+		  
+		   			},
+		   			{
+		   				"article-link": "#",
+		   				"article-description": "Welcome to Encyclopedia Modern Developer"
+		   
+		   			},
+		   		    {
+		   				"article-link": "#",
+		   				"article-description": "All Trappings"
+		  
+		   			},
+		   			{
+		   				"article-link": "#",
+		   				"article-description": "Getting Your Selected Articles Published on Encyclopedia"
+		   
+		   			},
+		   			{
+		   				"article-link": "#",
+		  				 "article-description": "Welcome to Encyclopedia Modern Developer"
+		   
+		   			},
+		   			{		   
+		   				"article-link": "#",
+		  
+		   			}
+		   		 ] //upcoming posts
+		   		   
+		  };
+		
+        
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+dataObject.txt
+Open with Google Docs
+Displaying dataObject.txt.
